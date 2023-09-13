88e178ebf185e6746cca408f77e9feb151d4bf11
SilviaGalvis <118777943+SilviaGalvis@users.noreply.github.com>
2023-09-12 21:50:35 -0500

    galeria

---
+++ js/01-gallery.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const createItem = (item) => { 
+    return  `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="Image description"
+    />
+  </a>
+</li > `
+}
+
+
+const itemsContainer = document.querySelector('.gallery')
+for (const item of galleryItems) {
+    const newItem = createItem(item);
+    itemsContainer.innerHTML += newItem;
+};
+
